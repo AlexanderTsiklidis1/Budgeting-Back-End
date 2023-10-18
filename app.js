9f7429b5a41580191c044b2593dd2aa549a7c3e3
Alexander Tsiklidis <alex@Alexanders-MacBook-Air.local>
2023-10-17 21:36:57 -0400

    makes changes in files for project setup

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const budgets = require("./controllers/budgetsController");
+
+const app = express();
+// const cors = require("cors")
+// app.use(cors())
+
+// app.use(express.json());
+// app.use("/budgets", budgets);
+
+const coolest = "Alex";
+
+app.get("/", (req, res) => {
+    console.log(coolest);
+    const upperCoolest = coolest.toUpperCase();
+    res.send(`Welcome to my express app.  ${upperCoolest} is the coolest. its official`);
+})
+
+app.get("*", (req, res) => {
+    res.status(404).json({error: "no page found "})
+})
+
+
+
+module.exports = app;
