a1bfcd01cac564b0062bf9419a39057479967b74
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2023-10-22 02:17:32 -0400

    changes going live console.log

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ require("dotenv").config();
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-    console.log(`%%%%%%%%!#%!#$%!%!$#%!%$#! we are live on port ${PORT} %%%%%%%%`)
+    console.log(`We are live on port ${PORT} `)
 });
